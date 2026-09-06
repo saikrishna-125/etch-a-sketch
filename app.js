f5678b0ec5a0f17d2da0761afe915a5eb7d73179
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-05 20:42:19 -0600

    Add functionality to change grid size
    Change color theme

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-const container = document.querySelector("#container");
+const body = document.querySelector("body");
+let container = document.querySelector(".container");
+const changeSizeBtn = document.querySelector("#change-size");
+
+let size = 16;
+
+let color = "#714955";
 
 // container dimensions
-width = 800;
-height = 800;
+const width = 800;
+const height = 800;
 
 container.style.width = `${width}px`;
 container.style.height = `${height}px`;
 
-grid = [];
+// adds (size*size) number of divs to container
+// flex properties set in css file
+// set even listeners for all div elements in container
+function setupGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${width / size}px`;
 
-let size = 16;
+    square.addEventListener("mouseover", (e) => {
+      if (e.buttons === 1) square.style.backgroundColor = color;
+    });
 
-for (let i = 0; i < size * size; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.style.width = `${width / size}px`;
+    square.addEventListener("mousedown", (e) => {
+      square.style.backgroundColor = color;
+    });
 
-  square.addEventListener("mouseover", (e) => {
-    if (e.buttons === 1) square.style.backgroundColor = "#00607a";
-  });
-
-  square.addEventListener("mousedown", (e) => {
-    square.style.backgroundColor = "#00607a";
-  });
-
-  grid.push(square);
-  container.appendChild(square);
+    container.appendChild(square);
+  }
 }
+
+// get size input
+// remove existing container
+// create new container and call setupGrid for it
+// assign newContainer to container;
+changeSizeBtn.addEventListener("click", () => {
+  size = +prompt("Enter a positive integer between 4 and 64:");
+
+  if (!(Number.isInteger(size) && size > 0) || !(size >= 4 && size <= 64)) {
+    alert("Invalid Input. Size set to default (16x16)");
+    size = 16;
+  }
+
+  container.remove();
+
+  let newContainer = document.createElement("div");
+  newContainer.classList.add("container");
+
+  newContainer.style.width = `${width}px`;
+  newContainer.style.height = `${height}px`;
+
+  body.appendChild(newContainer);
+
+  container = newContainer;
+
+  setupGrid(size);
+});
+
+setupGrid(size);
